28d6d1d991b86ff033c798779a2158e144c0b438
Farogh Zaidi <115211808+Faroghzaidi@users.noreply.github.com>
2023-04-17 14:20:10 +0530

    Update BasicController.java

--- src/main/java/com/lovetolearn/SpringBootApp/controller/BasicController.java
+++ src/main/java/com/lovetolearn/SpringBootApp/controller/BasicController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BasicController {
 
-	@GetMapping("/get")
+	@GetMapping("/zaidi")
 	public String getData() {
 		return "Welcome To My Running SpringAPP";
 	}
